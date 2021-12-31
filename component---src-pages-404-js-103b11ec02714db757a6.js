31f9125ae5b23c36550014d1f97dbe5aefb79235
kinhthu <letruongkinhthu@gmail.com>
2021-12-31 12:06:11 +0700

    Updates

---
+++ component---src-pages-404-js-103b11ec02714db757a6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(n,t,o){"use strict";o.r(t);var e=o("q1tI"),u=o.n(e);t.default=function(n){n.location;return u.a.createElement("h1",null,"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-103b11ec02714db757a6.js.map
