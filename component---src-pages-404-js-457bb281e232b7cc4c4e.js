9a892609aa7e384b8d5275d57acea02a29e560a4
kinhthu <letruongkinhthu@gmail.com>
2021-12-31 22:04:07 +0700

    Updates

---
+++ component---src-pages-404-js-457bb281e232b7cc4c4e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3MKT":function(n,t,o){n.exports=o.p+"static/notfound-3869a44d91bf6248b85fce2101563c54.png"},w2l6:function(n,t,o){"use strict";o.r(t);var c=o("q1tI"),a=o.n(c),e=o("3MKT"),i=o.n(e);t.default=function(n){n.location;return a.a.createElement("img",{src:i.a,alt:"not found"})}}}]);
+//# sourceMappingURL=component---src-pages-404-js-457bb281e232b7cc4c4e.js.map
